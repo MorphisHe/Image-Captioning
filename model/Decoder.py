084e80e4280765a3bb26749cb8820c6f85176924
jh8011nyu <jh8011@nyu.edu>
2022-12-16 07:04:26 -0500

    adding decoder

--- model/Decoder.py
+++ model/Decoder.py
@@ -1,31 +1,40 @@
 import torch
 from torch import nn
+from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
+
+
 
 class DecoderRNN(nn.Module):
-    def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1):
-        # code from: https://github.com/tatwan/image-captioning-pytorch/blob/main/model.py with modificati
+    # code from: https://github.com/tatwan/image-captioning-pytorch/blob/main/model.py with modification
+    def __init__(self, embed_size, hidden_size, vocab_size, num_layers):
         super(DecoderRNN, self).__init__()
-        self.embed_size = embed_size
-        self.hidden_size = hidden_size
-        self.vocab_size = vocab_size
-        self.num_layers = num_layers
+
+        # create layers
+        self.word_embedding = nn.Embedding(vocab_size, embed_size)
+        self.lstm = nn.LSTM(embed_size, hidden_size, num_layers, batch_first=True)
+        self.linear = nn.Linear(hidden_size, vocab_size)
+
+    def forward(self, features, captions, lengths):
+        '''
+        `features`: (batch_size, embed_size) encoded image feature from Encoder model
+        `captions`: (batch_size, padded_seq_length) encoded captions
+        `lengths`: (batch_size, ) lengths of each caption before padding
+        '''
+        # caption embedding
+        caption_embed = self.word_embedding(captions) # (batch_size, padded_seq_length, embed_size)
+
+        # ingestion step, appending image feature to caption embedding
+        # (batch_size, padded_seq_length+1, embed_size) 
+        caption_embed = torch.cat((features.unsqueeze(dim=1), caption_embed), 1)
         
-        self.word_embedding = nn.Embedding(self.vocab_size, self.embed_size)
-        self.lstm = nn.LSTM(self.embed_size, 
-                            self.hidden_size, 
-                            self.num_layers, 
-                            batch_first=True)
-        
-        self.fc = nn.Linear(self.hidden_size, self.vocab_size)
-        #self.hidden = (torch.zeros(1, 1, self.hidden_size), torch.zeros(1, 1, self.hidden_size))
-        
-    
-    def forward(self, features, captions):
-        
-        caption_embed = self.word_embedding(captions[:, :-1])
-        caption_embed = torch.cat((features.unsqueeze(dim=1), caption_embed),1)
-        output, self.hidden = self.lstm(caption_embed)
-        #print(output.shape)
-        output = self.fc(output)
-        #print(self.embed_size, self.vocab_size)
-        return output
+        # rnn
+        self.lstm.flatten_parameters()
+        packed_cap_embed = pack_padded_sequence(
+                                caption_embed, lengths, 
+                                batch_first=True,
+                                enforce_sorted=False) # data: (total_words[padded], embed_size)
+        hiddens, _ = self.lstm(packed_cap_embed) # data: (total_words[padded], hidden_size)
+        hiddens, _ = pad_packed_sequence(hiddens, batch_first=True) # unpack: (batch_size, padded_seq_length, hidden_size)
+        outputs = self.linear(hiddens) # (batch_size, padded_seq_length, vocab_size)
+
+        return outputs
