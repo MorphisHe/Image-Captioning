61650cc30a86bf439da90f371869978e9f84a3f5
jh8011nyu <jh8011@nyu.edu>
2022-12-16 03:54:35 -0500

    implemented get vocabulary

--- utils/utils.py
+++ utils/utils.py
@@ -1,5 +1,10 @@
+import sys
+sys.path.append("../")
+
 import os
+import pandas as pd
 from torch.utils.data import random_split
+from dataset.flickr30k import Vocabulary
 
 
 def get_data_split(image_dir, trainset_ratio=0.8):
@@ -19,8 +24,34 @@ def get_data_split(image_dir, trainset_ratio=0.8):
     return train, dev, test
 
 
+def get_vocabulary(label_path):
+    vocab = Vocabulary()
+
+    # get labels that belong to this dataset
+    df = pd.read_csv(label_path, delimiter="|")
+    records = df.to_dict("record")
+    for record in records:
+        # lowercase, remove special chars
+        print(record["image_name"])
+        print(record[" comment"])
+        caption = record[" comment"].strip()
+        tokens = caption.split()
+        tokens = [token.lower() for token in tokens if token.isalnum()]
+        
+        # add to vocab
+        for token in tokens:
+            vocab.add_word(token)
+    
+    return vocab
+
+
+#def get_dataloader()
+
 
 
 if __name__ == "__main__":
-    train, dev, test = get_data_split("data/flickr30k-images")
+    train, dev, test = get_data_split("../data/flickr30k-images")
     print(len(train), len(dev), len(test))
+
+    vocab = get_vocabulary("../data/results.csv")
+    print(len(vocab))
